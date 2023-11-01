99a267eea7789c0fdcb7196a52faebf0abe903ce
Яна Христова Методиева <stu2201261016@uni-plovdiv.bg>
2023-11-01 19:46:43 +0200

    Added WebMvcConfigurer to allow origin from http://localhost:3000 globally for the entire project.

--- e-gov-backend/src/main/java/com/egovernment/egovbackend/web/UserVoteController.java
+++ e-gov-backend/src/main/java/com/egovernment/egovbackend/web/UserVoteController.java
@@ -2,7 +2,6 @@ package com.egovernment.egovbackend.web;
 
 import com.egovernment.egovbackend.domain.dto.UserVotedInfoDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserVoteController {
 
-    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/api/votes")
     public ResponseEntity<UserVotedInfoDTO> getUserVoteData(@RequestBody UserVotedInfoDTO user){
         //userToBeAddedToTheDataBase
