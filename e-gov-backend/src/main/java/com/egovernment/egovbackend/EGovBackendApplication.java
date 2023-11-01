ce4653f74229287e9984421aa4fef497fff0faa9
Яна Христова Методиева <stu2201261016@uni-plovdiv.bg>
2023-11-01 17:07:04 +0200

    Added e-gov-backend from feature/BE-001

---
+++ e-gov-backend/src/main/java/com/egovernment/egovbackend/EGovBackendApplication.java
@@ -0,0 +1,13 @@
+package com.egovernment.egovbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EGovBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EGovBackendApplication.class, args);
+	}
+
+}
---
+++ e-gov-backend/src/test/java/com/egovernment/egovbackend/EGovBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egovernment.egovbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EGovBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
