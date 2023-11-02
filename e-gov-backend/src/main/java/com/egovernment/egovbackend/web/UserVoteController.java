7fffc6ba2b8ccef66b9f3c6c88eccd58a36f88ef
Яна Христова Методиева <stu2201261016@uni-plovdiv.bg>
2023-11-02 14:24:24 +0200

    OpenApiConfiguration added. Swagger documentation added in @RestController UserVoteController for method saveUserVoteData() (I have changed the name since the team lead told us that names starting with get are more appropriate for @GetMapping requests) You can reach the Swagger page at http://localhost:8080/swagger-ui/index.html

--- e-gov-backend/src/main/java/com/egovernment/egovbackend/web/UserVoteController.java
+++ e-gov-backend/src/main/java/com/egovernment/egovbackend/web/UserVoteController.java
@@ -1,6 +1,11 @@
 package com.egovernment.egovbackend.web;
 
 import com.egovernment.egovbackend.domain.dto.UserVotedInfoDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,8 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserVoteController {
 
+    @Operation(summary = "Receives and returns information about the user name and vote option")
+    @ApiResponses(
+            value = @ApiResponse(responseCode = "200",
+                    description = "The user data have been received successfully " +
+                            "and returned back to the response body so that they can be visualized.",
+                    content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = UserVotedInfoDTO.class))
+                    }
+            )
+    )
     @PostMapping("/api/votes")
-    public ResponseEntity<UserVotedInfoDTO> getUserVoteData(@RequestBody UserVotedInfoDTO user){
+    public ResponseEntity<UserVotedInfoDTO> saveUserVoteData(@RequestBody UserVotedInfoDTO user){
         //userToBeAddedToTheDataBase
         return ResponseEntity.ok(user);
     }
