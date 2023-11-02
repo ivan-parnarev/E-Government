3f4233d221b604606883173d6c82345bff17377a
Nedko Karaivanov <nedko2016@gmail.com>
2023-11-02 15:29:05 +0200

    Added UserService and UserRepository
    Created UserService with functionality to save and retrieve data from the DB. Created GetMapping in UserVoteController

--- e-gov-backend/src/main/java/com/egovernment/egovbackend/domain/dto/UserVotedInfoDTO.java
+++ e-gov-backend/src/main/java/com/egovernment/egovbackend/domain/dto/UserVotedInfoDTO.java
@@ -1,26 +1,17 @@
 package com.egovernment.egovbackend.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
 public class UserVotedInfoDTO {
 
     private String voterName;
+
     private String voteOption;
-
-    public String getVoterName() {
-        return voterName;
-    }
-
-    public UserVotedInfoDTO setVoterName(String voterName) {
-        this.voterName = voterName;
-        return this;
-    }
-
-    public String getVoteOption() {
-        return voteOption;
-    }
-
-    public UserVotedInfoDTO setVoteOption(String voteOption) {
-        this.voteOption = voteOption;
-        return this;
-    }
-
 }
--- e-gov-backend/src/main/java/com/egovernment/egovbackend/web/UserVoteController.java
+++ e-gov-backend/src/main/java/com/egovernment/egovbackend/web/UserVoteController.java
@@ -1,19 +1,26 @@
 package com.egovernment.egovbackend.web;
 
 import com.egovernment.egovbackend.domain.dto.UserVotedInfoDTO;
+import com.egovernment.egovbackend.services.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 public class UserVoteController {
 
+    private final UserService userService;
     @Operation(summary = "Receives and returns information about the user name and vote option")
     @ApiResponses(
             value = @ApiResponse(responseCode = "200",
@@ -26,8 +33,14 @@ public class UserVoteController {
     )
     @PostMapping("/api/votes")
     public ResponseEntity<UserVotedInfoDTO> saveUserVoteData(@RequestBody UserVotedInfoDTO user){
-        //userToBeAddedToTheDataBase
-        return ResponseEntity.ok(user);
+        UserVotedInfoDTO savedUser = this.userService.addNewUserVote(user);
+        return ResponseEntity.ok(savedUser);
+    }
+
+    @GetMapping("/api/votes")
+    public ResponseEntity<List<UserVotedInfoDTO>> getAllUserVotes() {
+        List<UserVotedInfoDTO> allSavedUserVotes = this.userService.getAllUserVotes();
+        return ResponseEntity.ok(allSavedUserVotes);
     }
 
 }
