edfa79413a70f115c936743833120555e3aa2cef
bbogomilov1 <bdbogomilov@gmail.com>
2023-11-04 11:26:04 +0200

    created home page which displays information about the platform

--- e-gov-frontend/src/App.js
+++ e-gov-frontend/src/App.js
@@ -2,11 +2,13 @@ import styles from "./App.module.css";
 import FormComponent from "./components/FormComponent";
 import ListComponent from "./components/ListComponent";
 import MainNavigation from "./components/MainNavigation";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className={styles.app}>
       <MainNavigation />
+      <HomePage />
       <div className={styles.container}>
         <FormComponent />
         <ListComponent />
--- e-gov-frontend/src/components/MainNavigation.js
+++ e-gov-frontend/src/components/MainNavigation.js
@@ -2,7 +2,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import styles from "./MainNavigation.module.css";
-import eGovLogo from "../assets/e-gov-logo.png";
+import eGovLogo from "../assets/images/e-gov-logo.png";
 
 function MainNavigation() {
   return (
@@ -15,13 +15,16 @@ function MainNavigation() {
           >
             <img src={eGovLogo} alt="logo" className={styles.navbarLogoImage} />
           </Navbar.Brand>
+
           <Nav className="me-auto">
             <Nav.Link href="#home" className={styles.navbarNavLink}>
               Начало
             </Nav.Link>
+
             <Nav.Link href="#register" className={styles.navbarNavLink}>
               Регистрация
             </Nav.Link>
+
             <Nav.Link
               href="#active-campaigns"
               className={`${styles.navbarNavLink} ${styles.activeCampaigns}`}
