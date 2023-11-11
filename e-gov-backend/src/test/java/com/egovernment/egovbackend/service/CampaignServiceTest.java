47150a1f6e5118c7662f7f9a12889a2c2ec3246d
Яна Христова Методиева <stu2201261016@uni-plovdiv.bg>
2023-11-11 14:57:18 +0200

    Completed test cases for campaign service.

--- e-gov-backend/src/test/java/com/egovernment/egovbackend/service/CampaignServiceTest.java
+++ e-gov-backend/src/test/java/com/egovernment/egovbackend/service/CampaignServiceTest.java
@@ -1,10 +1,12 @@
 package com.egovernment.egovbackend.service;
 
+import com.egovernment.egovbackend.domain.dto.CampaignViewDTO;
 import com.egovernment.egovbackend.domain.entity.Campaign;
 import com.egovernment.egovbackend.domain.entity.Role;
 import com.egovernment.egovbackend.domain.entity.User;
 import com.egovernment.egovbackend.domain.enums.CampaignType;
 import com.egovernment.egovbackend.domain.enums.RoleEnum;
+import com.egovernment.egovbackend.domain.factory.CampaignFactory;
 import com.egovernment.egovbackend.repository.CampaignRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,8 +15,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 
+import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,6 +30,8 @@ public class CampaignServiceTest {
     private UserService userService;
     @Mock
     private RoleService roleService;
+    @Mock
+    private CampaignFactory campaignFactory;
     @Mock
     private ModelMapper modelMapper;
     private CampaignService campaignServiceToTest;
@@ -54,6 +60,86 @@ public class CampaignServiceTest {
         when(this.campaignRepository.count()).thenReturn(1L);
         this.campaignServiceToTest.initSampleCampaign();
         verify(campaignRepository, never()).save(any(Campaign.class));
+    }
+
+    @Test
+    void launchCampaignCreatesCampaign(){
+        User testUserFrom = User.builder().name("TestUser").build();
+
+        Campaign campaign = Campaign.builder()
+                .campaignType(CampaignType.VOTING)
+                .campaignTopic("Campaign Test Topic")
+                .from(testUserFrom)
+                .duration(1)
+                .answersJson("AnswersTest")
+                .build();
+
+        Campaign resultCampaign = this.campaignServiceToTest.launchCampaign(CampaignType.VOTING,
+                "Campaign Test Topic", testUserFrom,1, "AnswersTest");
+
+        assertNotNull(resultCampaign);
+        assertEquals(campaign.getCampaignType(), resultCampaign.getCampaignType());
+        assertEquals(campaign.getCampaignTopic(), resultCampaign.getCampaignTopic());
+        assertEquals(campaign.getFrom(), resultCampaign.getFrom());
+        assertEquals(campaign.getDuration(), resultCampaign.getDuration());
+        assertEquals(campaign.getAnswersJson(), resultCampaign.getAnswersJson());
+
+    }
+
+    @Test
+    void getActiveCampaignsReturnListOfCampaignDTOs() {
+
+        Campaign campaign = Campaign.builder()
+                .campaignType(CampaignType.VOTING)
+                .campaignTopic("Campaign Test Topic")
+                .duration(1)
+                .answersJson("AnswersTest")
+                .build();
+
+        CampaignViewDTO campaignViewDTO = CampaignViewDTO.builder()
+                .campaignType("VOTING")
+                .campaignTopic("Campaign Test Topic")
+                .duration(1)
+                .answersJson("AnswersTest")
+                .build();
+
+        when(campaignRepository.findAll()).thenReturn(List.of(campaign));
+        when(modelMapper.map(campaign, CampaignViewDTO.class)).thenReturn(campaignViewDTO);
+
+        List<CampaignViewDTO> campaigns = this.campaignServiceToTest.getActiveCampaigns();
+
+        assertFalse(campaigns.isEmpty());
+        assertEquals(1, campaigns.size());
+        assertEquals(campaignViewDTO.getCampaignTopic(), campaigns.get(0).getCampaignTopic());
+        assertEquals(campaignViewDTO.getCampaignType(), campaigns.get(0).getCampaignType());
+        assertEquals(campaignViewDTO.getDuration(), campaigns.get(0).getDuration());
+        assertEquals(campaignViewDTO.getAnswersJson(), campaigns.get(0).getAnswersJson());
+    }
+    @Test
+    void testGetCampaignByExistingIdReturnsTheRightCampaign(){
+        Campaign expectedCampaign = Campaign.builder()
+                .campaignType(CampaignType.VOTING)
+                .campaignTopic("Campaign Test Topic")
+                .build();
+
+        when(this.campaignRepository.findById(1L)).thenReturn(Optional.of(expectedCampaign));
+
+        Optional<Campaign> campaign = this.campaignServiceToTest.getCampaignById(1L);
+
+        assertTrue(campaign.isPresent());
+        assertEquals(expectedCampaign, campaign.get());
+        assertEquals(expectedCampaign.getCampaignTopic(), campaign.get().getCampaignTopic());
+        assertEquals(expectedCampaign.getCampaignType(), campaign.get().getCampaignType());
+
+    }
+
+    @Test
+    void testGetCampaignByNonExistingIdReturnsEmptyOptional(){
+
+        when(this.campaignRepository.findById(1L)).thenReturn(Optional.empty());
+        Optional<Campaign> campaign = this.campaignServiceToTest.getCampaignById(1L);
+        assertTrue(campaign.isEmpty());
+
     }
 
 }
