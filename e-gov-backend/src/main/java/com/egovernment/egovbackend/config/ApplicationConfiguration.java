bfa333657f5cd0e1b245fb6ca48aa8bde1307236
Яна Христова Методиева <stu2201261016@uni-plovdiv.bg>
2023-11-12 00:32:31 +0200

    Fixed bug for Location being null. Explicitly should tell the cross origin that we allow the Location header to be exposed.

--- e-gov-backend/src/main/java/com/egovernment/egovbackend/config/ApplicationConfiguration.java
+++ e-gov-backend/src/main/java/com/egovernment/egovbackend/config/ApplicationConfiguration.java
@@ -13,7 +13,8 @@ public class ApplicationConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .exposedHeaders("Location"); ;
     }
 
     @Bean
