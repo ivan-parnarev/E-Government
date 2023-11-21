070fd7d811a4dcc79ffd186c151277b1375d7596
Kaloyan <milchevkaloian@gmail.com>
2023-11-21 21:26:00 +0200

    initializes database

--- e-gov-dev-env/env/init.sql
+++ e-gov-dev-env/env/init.sql
@@ -1,10 +1,18 @@
-CREATE DATABASE "e-gov-db";
+-- Check if the database exists
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'e-gov-db') THEN
+        -- Create the database
+        CREATE DATABASE "e-gov-db";
+    END IF;
+END $$;
 
--- Switch to the newly created database
-\c e-gov-db;
+-- Switch to the newly created or existing database
+\c "e-gov-db";
 
 -- Create a table if it doesn't exist
 CREATE TABLE IF NOT EXISTS test (
     id serial PRIMARY KEY,
     name VARCHAR (100) NOT NULL
 );
+
