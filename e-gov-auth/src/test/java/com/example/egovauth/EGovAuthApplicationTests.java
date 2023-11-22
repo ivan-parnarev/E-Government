8beb4a29565a6781fbae97a86e6d02496276fb6f
Nedko Karaivanov <nedko2016@gmail.com>
2023-11-22 17:40:31 +0200

    Initialized Authentication Service

---
+++ e-gov-auth/src/test/java/com/example/egovauth/EGovAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.egovauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EGovAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
