0dcfae9b1d87e000d62e57d63f1320d5bdf438cf
Яна Христова Методиева <stu2201261016@uni-plovdiv.bg>
2023-12-18 16:54:08 +0200

    Credentials are received from the application.yml file.

--- e-government-access-control/src/main/java/com/example/egovernmentaccesscontrol/database/DatabaseInitializer.java
+++ e-government-access-control/src/main/java/com/example/egovernmentaccesscontrol/database/DatabaseInitializer.java
@@ -1,25 +1,34 @@
 package com.example.egovernmentaccesscontrol.database;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
 import java.sql.*;
 
+@Component
+@RequiredArgsConstructor
 public class DatabaseInitializer {
-    private static final String DB_URL =
-            "jdbc:postgresql://localhost:5432/e-gov-db";
-    private static final String USER = "postgres";
-    private static final String PASS = "1234";
-    private static final String SUBSCRIPTION_NAME = "e_government_subscription".toLowerCase();
-    private static final String CHECK_SUBSCRIPTION_SQL =
+    private final DataSourceProperties dataSourceProperties;
+    private final String SUBSCRIPTION_NAME = "e_government_subscription".toLowerCase();
+    private final String CHECK_SUBSCRIPTION_SQL =
             "SELECT 1 FROM pg_subscription WHERE subname = ?";
-    private static final String CHECK_REPLICATION_SLOT_SQL =
+    private final String CHECK_REPLICATION_SLOT_SQL =
             "SELECT 1 FROM pg_replication_slots WHERE slot_name = ?";
-    private static final String CREATE_SUBSCRIPTION_SQL =
-            "CREATE SUBSCRIPTION " + SUBSCRIPTION_NAME + " " +
-                    "CONNECTION 'host=localhost port=5432 dbname=e-gov-db user=postgres password=1234' " +
-                    "PUBLICATION e_government_campaigns_publication " +
-                    "WITH (synchronous_commit = 'off', copy_data = true, enabled = true);";
+    private String createSubscriptionSQL() {
+        return "CREATE SUBSCRIPTION " + SUBSCRIPTION_NAME + " " +
+                "CONNECTION 'host=localhost port=5432 dbname=e-gov-db user=" +
+                dataSourceProperties.getUsername() + " password=" +
+                dataSourceProperties.getPassword() + " ' " +
+                "PUBLICATION e_government_campaigns_publication " +
+                "WITH (synchronous_commit = 'off', copy_data = true, enabled = true);";
+    }
 
-    public static void checkAndCreateSubscription() {
-        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+    public void checkAndCreateSubscription() {
+        try (Connection conn = DriverManager.getConnection(
+                dataSourceProperties.getUrl(),
+                dataSourceProperties.getUsername(),
+                dataSourceProperties.getPassword());
+
              PreparedStatement checkSubscriptionStmt = conn.prepareStatement(CHECK_SUBSCRIPTION_SQL);
              PreparedStatement checkSlotStmt = conn.prepareStatement(CHECK_REPLICATION_SLOT_SQL)) {
 
@@ -35,7 +44,7 @@ public class DatabaseInitializer {
                 ResultSet slotRs = checkSlotStmt.executeQuery();
                 if (!slotRs.next()) {
                     try (Statement createSubStmt = conn.createStatement()) {
-                        createSubStmt.execute(CREATE_SUBSCRIPTION_SQL);
+                        createSubStmt.execute(createSubscriptionSQL());
                         System.out.println("Subscription created successfully.");
                     }
                 } else {
