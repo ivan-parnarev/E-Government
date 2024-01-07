5929298f0608135923ba4982fec8cd97f5e8b977
Яна Христова Методиева <stu2201261016@uni-plovdiv.bg>
2024-01-07 18:17:52 +0200

    Added documentation for Access-Control Service controller endpoint.

--- e-government-access-control/src/main/java/com/example/egovernmentaccesscontrol/web/CampaignController.java
+++ e-government-access-control/src/main/java/com/example/egovernmentaccesscontrol/web/CampaignController.java
@@ -4,6 +4,10 @@ import com.example.egovernmentaccesscontrol.domain.dto.CampaignFilteredDTO;
 import com.example.egovernmentaccesscontrol.service.CampaignService;
 import com.example.egovernmentaccesscontrol.web.interfaces.CampaignControllerInterface;
 import com.example.egovernmentaccesscontrol.web.path.ApiPaths;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +21,10 @@ public class CampaignController implements CampaignControllerInterface {
 
     private final CampaignService campaignService;
 
+    @Operation(summary = "Get Active Campaigns", description = "Retrieves a list of all active voting campaigns available.")
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved the list of active campaigns",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = CampaignFilteredDTO.class)))
     @Override
     @GetMapping(ApiPaths.ACTIVE_PATH)
     public ResponseEntity<List<CampaignFilteredDTO>> getActiveCampaigns(){
