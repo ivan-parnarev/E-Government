e0a5d6146ac3cca379ca7cffa9d677aaa8b114be
Яна Христова Методиева <stu2201261016@uni-plovdiv.bg>
2024-01-12 17:05:25 +0200

    OpenApi configuration committed again

--- e-government-access-control/src/main/java/com/example/egovernmentaccesscontrol/config/OpenApiConfiguration.java
+++ e-government-access-control/src/main/java/com/example/egovernmentaccesscontrol/config/OpenApiConfiguration.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfiguration {
-
     @Bean
     public OpenAPI openAPI(){
         return new OpenAPI()
