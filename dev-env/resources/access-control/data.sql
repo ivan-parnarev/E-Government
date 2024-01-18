a4b4ac4174aab26ca0140f9b30a3de54cb74feb7
kaloyan <milchevkaloian@gmail.com>
2024-01-18 21:15:03 +0200

    moved init files in directory resources/access-control

---
+++ dev-env/resources/access-control/data.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE "e-government-access-control";
+ALTER ROLE postgres SET search_path TO 'e-government-access-control';
+CREATE SCHEMA IF NOT EXISTS e_gov_db;
+ALTER ROLE postgres SET search_path TO e_gov_db;
