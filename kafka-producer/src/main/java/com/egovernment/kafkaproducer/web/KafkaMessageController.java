63b37e7eb5f8c72b30fd7c64da00056f23d1f46d
Яна Христова Методиева <stu2201261016@uni-plovdiv.bg>
2024-01-26 12:16:00 +0200

    Documented endpoints for kafka producer.

--- kafka-producer/src/main/java/com/egovernment/kafkaproducer/web/KafkaMessageController.java
+++ kafka-producer/src/main/java/com/egovernment/kafkaproducer/web/KafkaMessageController.java
@@ -4,6 +4,11 @@ import com.egovernment.kafkaproducer.domain.entity.UserVotedInfoDTO;
 import com.egovernment.kafkaproducer.response.ApiCustomResponse;
 import com.egovernment.kafkaproducer.service.KafkaService;
 import com.egovernment.kafkaproducer.web.interfaces.KafkaMessageControllerInterface;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +21,16 @@ public class KafkaMessageController implements KafkaMessageControllerInterface {
 
     private final KafkaService kafkaSendMessageService;
 
+    @Operation(summary = "Send Kafka Message",
+            description = "Sends a message to a Kafka topic based on the campaign title provided in the message.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Message successfully sent to Kafka topic",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiCustomResponse.class))}),
+            @ApiResponse(responseCode = "500",
+                    description = "Internal Server Error")
+    })
     @Override
     @PostMapping("/send")
     public ResponseEntity<ApiCustomResponse> sendMessage
--- kafka-producer/src/main/java/com/egovernment/kafkaproducer/web/KafkaTopicController.java
+++ kafka-producer/src/main/java/com/egovernment/kafkaproducer/web/KafkaTopicController.java
@@ -4,6 +4,11 @@ import com.egovernment.kafkaproducer.domain.dto.CampaignsTopicDTO;
 import com.egovernment.kafkaproducer.response.ApiCustomResponse;
 import com.egovernment.kafkaproducer.service.KafkaService;
 import com.egovernment.kafkaproducer.web.interfaces.KafkaTopicControllerInterface;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +20,16 @@ public class KafkaTopicController implements KafkaTopicControllerInterface {
 
     private final KafkaService kafkaService;
 
+    @Operation(summary = "Create Kafka Topics",
+            description = "Creates Kafka topics for each campaign title provided in the request.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Topics successfully created",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiCustomResponse.class))}),
+            @ApiResponse(responseCode = "500",
+                    description = "Internal Server Error")
+    })
     @Override
     @PostMapping("/create")
     public ResponseEntity<ApiCustomResponse> createCampaignsTopic(CampaignsTopicDTO campaignsTopicDTO) {
