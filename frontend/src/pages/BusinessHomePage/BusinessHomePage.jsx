b9a82ee13ce44deccb69926cf39ad70cc9edd8f4
Chavdar <chokobg@gmail.com>
2024-03-07 15:30:07 +0200

    tweaked responsive ranges and home page

--- frontend/src/pages/BusinessHomePage/BusinessHomePage.jsx
+++ frontend/src/pages/BusinessHomePage/BusinessHomePage.jsx
@@ -1,3 +1,5 @@
+import styles from './BusinessHomePage.module.css';
+
 export default function BusinessHomePage() {
-    return <div>BusinessHomePage</div>;
+    return <h1 className={styles.heading}>Comming soon...</h1>;
 }
